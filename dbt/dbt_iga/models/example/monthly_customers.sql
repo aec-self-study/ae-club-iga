0b969b11187e923e48393a0b2696fcf02f9b6295
iggynoid <126175330+iggynoid@users.noreply.github.com>
2023-04-15 17:45:10 +0000

    replace sql with ref function

--- dbt/dbt_iga/models/example/monthly_customers.sql
+++ dbt/dbt_iga/models/example/monthly_customers.sql
@@ -1,5 +1,2 @@
-select
-    date_trunc(first_order_at, month) as signup_month,
-    count(*) as new_customers
-from `aec-students.dbt_iga.customers` 
+from {{ref ('customers')}}
 group by signup_month
